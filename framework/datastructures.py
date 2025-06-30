b8a1d1064dedbba002b2496be1485c8201e7b7ee
Monster0506 <tjraklovits@gmail.com>
2025-06-30 13:34:19 -0400

    Add proper typing to ListNode

--- framework/datastructures.py
+++ framework/datastructures.py
@@ -2,7 +2,7 @@ from typing import Optional, List, Any
 
 
 class ListNode:
-    def __init__(self, val=0, next=None):
+    def __init__(self, val: int | None = 0, next=None):
         self.val = val
         self.next = next
 
